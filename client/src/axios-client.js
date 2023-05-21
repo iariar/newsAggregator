b9161f762337e1dd5b1705a9e02c088565499dc3
Iariss <58819283+iariar@users.noreply.github.com>
2023-05-21 01:02:44 +0100

    Update axios-client.js

--- client/src/axios-client.js
+++ client/src/axios-client.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient = axios.create({
-    baseURL: `http://192.168.204.71`
+    baseURL: `http://127.0.0.1:8000`
 })
     axiosClient.interceptors.request.use((config) => 
         {
